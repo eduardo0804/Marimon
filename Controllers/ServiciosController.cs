e360456aeedeb7ee19143607da84f3c836c1c689
Carlo <carlofav12@gmail.com>
2025-05-01 21:32:44 -0500

    Añadir información de usuario en la vista de reserva servicio

--- Controllers/ServiciosController.cs
+++ Controllers/ServiciosController.cs
@@ -8,6 +8,7 @@ using Marimon.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Identity;
 
 namespace Marimon.Controllers
 {
@@ -15,11 +16,13 @@ namespace Marimon.Controllers
     {
         private readonly ILogger<ServiciosController> _logger;
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager; 
 
-        public ServiciosController(ILogger<ServiciosController> logger, ApplicationDbContext context)
+        public ServiciosController(ILogger<ServiciosController> logger, ApplicationDbContext context,UserManager<IdentityUser> userManager)
         {
             _logger = logger;
             _context = context;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -28,15 +31,36 @@ namespace Marimon.Controllers
             return View(servicios);
         }
 
-        public IActionResult Detalle(int id)
+        public async Task<IActionResult> Detalle(int id)
+    {
+        var servicio = _context.Servicio.Find(id);
+        if (servicio == null)
         {
-            var servicio = _context.Servicio.Find(id);
-            if (servicio == null)
-            {
-                return NotFound();
-            }
-            return View("Servicio", servicio);
+            return NotFound();
         }
+
+        string nombre = "";
+        string apellido = "";
+
+        if (User.Identity.IsAuthenticated)
+    {
+        var identityUser = await _userManager.GetUserAsync(User);
+        if (identityUser != null)
+        {
+            var usuario = _context.Usuario.FirstOrDefault(u => u.usu_id == identityUser.Id);
+            if (usuario != null)
+            {
+                nombre = usuario.usu_nombre ?? "";
+                apellido = usuario.usu_apellido ?? "";
+            }
+        }
+    }
+
+        ViewBag.Nombre = nombre;
+        ViewBag.Apellido = apellido;
+
+        return View("Servicio", servicio);
+    }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
