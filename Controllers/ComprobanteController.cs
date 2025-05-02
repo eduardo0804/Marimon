64dfa3e151c6cc83d537823d942548da7173fd9a
Adriancas28 <castroadrian1228@gmail.com>
2025-05-01 20:58:12 -0500

    Usuario - Boleta -Factura

--- Controllers/ComprobanteController.cs
+++ Controllers/ComprobanteController.cs
@@ -3,23 +3,39 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Marimon.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace Marimon.Controllers
 {
     public class ComprobanteController : Controller
     {
+        private readonly ApplicationDbContext _context;
         private readonly ILogger<ComprobanteController> _logger;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public ComprobanteController(ILogger<ComprobanteController> logger)
+    
+
+        public ComprobanteController(ApplicationDbContext context,ILogger<ComprobanteController> logger,UserManager<IdentityUser> userManager)
         {
+            _context = context;
+            _userManager = userManager;
             _logger = logger;
         }
+        
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var identityUserId = _userManager.GetUserId(User);
+            var usuario = await _context.Usuarios.FirstOrDefaultAsync(u => u.usu_id == identityUserId);
+            if (usuario == null)
+            {
+                return RedirectToAction("Login", "Account"); // o muestra un mensaje
+            }
+            return View(usuario);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
