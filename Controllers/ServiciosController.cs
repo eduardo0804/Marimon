aff650dcc2b722ce5d45ef688dc8fc3750d8d8ea
JeanClix <jp10.02.2003@gmail.com>
2025-05-02 16:43:35 -0500

    update detalles

--- Controllers/ServiciosController.cs
+++ Controllers/ServiciosController.cs
@@ -1,14 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Marimon.Data;
 using Marimon.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Identity;
 
 namespace Marimon.Controllers
 {
