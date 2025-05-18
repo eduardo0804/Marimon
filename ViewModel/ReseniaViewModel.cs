667ec65e516bf5b1836a8e8c8f355339c9fabd92
Usmpmarkp <sebasklidad107@gmail.com>
2025-05-17 23:06:36 -0500

    Añadiendo Reseña de Autoparte

---
+++ ViewModel/ReseniaViewModel.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Marimon.ViewModel
+{
+    public class ReseniaViewModel
+    {
+        public int res_id { get; set; }
+        public string res_comentario { get; set; }
+        public int res_puntuacion { get; set; }
+        public DateTime res_fecha { get; set; }
+        public string res_gusto { get; set; }
+        public string usuario_id { get; set; }
+        public string usuario_nombre { get; set; }
+    }
+}
